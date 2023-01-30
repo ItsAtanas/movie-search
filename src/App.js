b7cf549b32c602606f5eb166799f090964ac8f81
ItsAtanas <bogdanoskiatanas@gmail.com>
2023-01-29 19:33:13 -0500

    the rest of it

--- src/App.js
+++ src/App.js
@@ -6,14 +6,6 @@ import MovieCard from "./MovieCard";
 
 const API_URL = 'https://www.omdbapi.com?apikey=866e0f4c';
 
-const movie1 = {
-    "Title": "Spiderman the Verse",
-    "Year": "2019–",
-    "imdbID": "tt12122034",
-    "Type": "series",
-    "Poster": "https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg"
-}
-
 const App = () => {
 
     const [movies, setMovies] = useState([]);
